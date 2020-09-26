4c0f1a3494dc1f85f9141072af60dd212e6ed307
Ankur Gupta <rukna1000@gmail.com>
2020-09-26 13:19:03 -0400

    Disable cider functionality for the minecraft mode

--- keymap.c
+++ keymap.c
@@ -818,7 +818,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |					 |			|			 |			|			 |			|				|						|				|			 |			|			 |			|			 |					 |
  * |-----------+------+------+------+------+------|				|						|				|------+------+------+------+------+-----------|
  * |					 |			|			 |			|			 |			|				|						|				|			 |			|			 |			|			 |					 |
- * |	l shift	 |			|			 |			|			 |			|				|						|				|			 |			|			 |			|			 |					 |
+ * |	l shift	 |			|			 |	c 	|			 |			|				|						|				|			 |			|			 |			|			 |					 |
  * |					 |			|			 |			|			 |			|				|						|				|			 |			|			 |			|			 |					 |
  * `-----------+------+------+------+------+--------------'						` -------------+------+------+------+------+-----------'
  *			|			 |			|			 |			|			 |																				 |			|			 |			|			 |			|
@@ -844,7 +844,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			KC_TRANSPARENT,  KC_1, 	          KC_2,            KC_3,	          KC_4,            KC_5,	          KC_6,
 			KC_TAB,          KC_Q,	          KC_W,            KC_E,	          KC_TRANSPARENT,  KC_TRANSPARENT,	KC_TRANSPARENT,
 			KC_ESCAPE,       KC_A,	          KC_S,            KC_D,	          KC_F,            KC_TRANSPARENT,
-			KC_LSHIFT,       KC_TRANSPARENT,	KC_TRANSPARENT,  KC_TRANSPARENT,	KC_TRANSPARENT,  KC_TRANSPARENT,	KC_TRANSPARENT,
+			KC_LSHIFT,       KC_TRANSPARENT,	KC_TRANSPARENT,  KC_C,	          KC_TRANSPARENT,  KC_TRANSPARENT,	KC_TRANSPARENT,
 			TO(BASE),        KC_F2,	          KC_F3,           KC_F5,	          KC_F11,
 			
 																																				//Left thumb cluster
